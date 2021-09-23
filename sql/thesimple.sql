f470ed9f5e7be20c16c6b5154bfa4d3acc15ba5c
Emil A. Siemes <digitalemil@googlemail.com>
2021-09-23 11:41:28 +0200

    Update thesimple.sql

--- sql/thesimple.sql
+++ sql/thesimple.sql
@@ -1,10 +1,5 @@
 
 
-Create database thegym;
-Create Role thegym With Superuser;
-Alter Role thegym With Login;
-\c thegym
-
 CREATE TABLE hrdata (id BIGINT, color text, location text, event_timestamp text, deviceid text, username text, heartrate INT, region text, PRIMARY KEY (id, region)) PARTITION BY LIST (region);
 
 CREATE TABLE latesthrdata (id BIGINT, color text, location text, event_timestamp text, deviceid text, username text, heartrate INT, region text, PRIMARY KEY (username, region)) PARTITION BY LIST (region);
