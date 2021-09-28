a90511cb28ed56d59a5a4687e560a0c6e758832c
Emil A. Siemes <digitalemil@googlemail.com>
2021-09-28 09:00:42 +0200

    Update thesimple.sql

--- sql/thesimple.sql
+++ sql/thesimple.sql
@@ -37,12 +37,6 @@ CREATE TABLE latesthrdata_apac
     (id, color, location, event_timestamp, deviceid, username, heartrate, region)
     FOR VALUES IN ('APAC');
 
-Insert into hrdata values (4711, '0x00FF00', '48.137154,11.576124', '1632389493615', 'my garmin', 'me',
-120, 'EMEA');
-Insert into hrdata values (4712, '0x808000', '37.775,-122.4183333', '1632389493615', 'my garmin', 'you',
-120, 'AMER');
-Insert into hrdata values (4713, '0xFF0000', '1.295078, 103.854570', '1632389493615', 'my garmin', 'it',
-120, 'APAC');
 
 
 
